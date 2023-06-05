5fcb941a4398eee330cdcd910f1ecf9245125b95
carlo <carlos.saico.mmc@gmail.com>
2023-06-05 11:36:19 -0500

    kkm

--- catalogo/catalogo/src/main/java/com/catalogo/catalogo/CatalogoApplication.java
+++ catalogo/catalogo/src/main/java/com/catalogo/catalogo/CatalogoApplication.java
@@ -2,9 +2,14 @@ package com.catalogo.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 @SpringBootApplication
 public class CatalogoApplication {
+
+	@GetMapping("/inicio")
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
