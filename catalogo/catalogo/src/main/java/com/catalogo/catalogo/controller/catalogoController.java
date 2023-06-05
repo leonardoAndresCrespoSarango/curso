d4ab1a9d1d476d8c065156f904e743beb7dd9ce1
carlo <carlos.saico.mmc@gmail.com>
2023-06-05 17:58:04 -0500

    get-post-put(pathc)-catalogo

--- catalogo/catalogo/src/main/java/com/catalogo/catalogo/controller/catalogoController.java
+++ catalogo/catalogo/src/main/java/com/catalogo/catalogo/controller/catalogoController.java
@@ -1,8 +1,50 @@
 package com.catalogo.catalogo.controller;
 
+import com.catalogo.catalogo.entity.catalogo;
+import com.catalogo.catalogo.services.catalogoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/catalogo")
 public class catalogoController {
+
+    private catalogoService catService;
+
+    @Autowired
+    public catalogoController(catalogoService catService) {
+        this.catService = catService;
+    }
+
+    @GetMapping("/todos")
+    public List<catalogo> obtenerTodosLosItems() {
+        return catService.getListaTodo();
+    }
+
+    @PostMapping("/crear")
+    public void crearCatalogo(@RequestBody catalogo catalogo) {
+        catService.insertarCatalogo(catalogo);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<String> actualizarCatalogo(@PathVariable("id") int id, @RequestBody catalogo catalogoActualizado) {
+        catalogo catalogoActualizadoResultado = catService.actualizarCatalogo(id, catalogoActualizado);
+        if (catalogoActualizadoResultado != null) {
+            return ResponseEntity.ok("Catálogo actualizado exitosamente");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- catalogo/catalogo/src/main/java/com/catalogo/catalogo/services/catalogoService.java
+++ catalogo/catalogo/src/main/java/com/catalogo/catalogo/services/catalogoService.java
@@ -1,8 +1,46 @@
 package com.catalogo.catalogo.services;
-import java.util.List;
+
+import com.catalogo.catalogo.entity.catalogo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
+@Transactional
 public class catalogoService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void insert(catalogo catalogo) {
+        entityManager.persist(catalogo);
+    }
+
+    public List<catalogo> getListaTodo() {
+        String jpql = "SELECT c FROM catalogo c";
+        Query query = entityManager.createQuery(jpql, catalogo.class);
+        List<catalogo> lista = query.getResultList();
+        return lista;
+    }
+
+    public void insertarCatalogo(catalogo catalogo) {
+        entityManager.persist(catalogo);
+    }
+
+    public catalogo actualizarCatalogo(int id, catalogo catalogoActualizado) {
+        catalogo catalogoExistente = entityManager.find(catalogo.class, id);
+        if (catalogoExistente != null) {
+            catalogoExistente.setNombre(catalogoActualizado.getNombre());
+            catalogoExistente.setDescripccion(catalogoActualizado.getDescripccion());
+            catalogoExistente.setRegistroId(catalogoActualizado.getRegistroId());
+            entityManager.merge(catalogoExistente);
+        }
+        return catalogoExistente;
+    }
 }
