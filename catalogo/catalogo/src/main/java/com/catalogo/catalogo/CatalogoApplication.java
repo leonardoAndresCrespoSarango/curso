c2bc610207818fd7a73539bd2b405810aba90ae9
carlo <carlos.saico.mmc@gmail.com>
2023-06-05 12:13:19 -0500

    cambio-inicial

--- catalogo/catalogo/src/main/java/com/catalogo/catalogo/CatalogoApplication.java
+++ catalogo/catalogo/src/main/java/com/catalogo/catalogo/CatalogoApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @RestController
 @SpringBootApplication
 public class CatalogoApplication {
 
 	@GetMapping("/inicio")
-
+	public String getMnesaje(){
+		return "HOLA FUNCIONA O NO FUNCIONA";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
 	}
