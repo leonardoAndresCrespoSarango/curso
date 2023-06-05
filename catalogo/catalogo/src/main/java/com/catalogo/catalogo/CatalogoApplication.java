0ec7d33360e38ba2f8bb628ac9973e48986ccf34
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2023-06-05 12:32:36 -0500

    microser

--- catalogo/catalogo/src/main/java/com/catalogo/catalogo/CatalogoApplication.java
+++ catalogo/catalogo/src/main/java/com/catalogo/catalogo/CatalogoApplication.java
@@ -2,12 +2,18 @@ package com.catalogo.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
 @SpringBootApplication
 public class CatalogoApplication {
 
+	@GetMapping("/inicio")
+	public String getMnesaje(){
+		return "HOLA FUNCIONA O NO FUNCIONA";
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogoApplication.class, args);
+		SpringApplication.run(CatalogoApplication.class,args);
 	}
 
 }
